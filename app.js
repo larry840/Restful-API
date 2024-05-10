46bc5e13391f732f4368c885b48e9346d54eb68b
Larry <trumpet.0417@gmail.com>
2024-05-10 17:45:02 +0800

    0510

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const mongoose = require("mongoose");
 const Student = require("./models/student.js");
 
-app.set("view engine", "ejs");
-
 mongoose
     .connect("mongodb://127.0.0.1/exampleDB")
     .then(() => {
@@ -13,6 +11,10 @@ mongoose
     .catch(e => {
         console.log(e);
     });
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/students", async (req, res) => {
     try {
@@ -30,6 +32,28 @@ app.get("/students/:_id", async (req, res) => {
         return res.send(foundStudent);
     } catch (e) {
         return res.status(500).send("尋找資料時發生錯誤");
+    }
+});
+
+app.post("/students", async (req, res) => {
+    try {
+        let { name, age, major, merit, other } = req.body;
+        let newStudent = new Student({
+            name,
+            age,
+            major,
+            scholarship: {
+                merit,
+                other,
+            },
+        });
+        let savedStudent = await newStudent.save();
+        return res.send({
+            msg: "資料儲存成功",
+            savedObject: savedStudent,
+        });
+    } catch (e) {
+        return res.status(500).send("儲存資料時發生錯誤");
     }
 });
 
