a76b805bf1ad67b7b595fc861a3e0634a8c2b178
Larry <trumpet.0417@gmail.com>
2024-05-15 15:07:58 +0800

    0515

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Student = require("./models/student");
+const methodOverride = require("method-override");
 
 mongoose
     .connect("mongodb://127.0.0.1/exampleDB")
@@ -15,6 +16,7 @@ mongoose
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/students", async (req, res) => {
     try {
@@ -43,13 +45,26 @@ app.get("/students/:_id", async (req, res) => {
     }
 });
 
+app.get("/students/:_id/edit", async (req, res) => {
+    let { _id } = req.params;
+    try {
+        let foundStudent = await Student.findOne({ _id }).exec();
+        if (foundStudent != null) {
+            return res.render("edit-student", { foundStudent });
+        } else {
+            return res.status(400).render("student-not-found");
+        }
+    } catch (e) {
+        return res.status(400).render("student-not-found");
+    }
+});
+
 app.post("/students", async (req, res) => {
     try {
-        let { name, age, major, merit, other } = req.body;
+        let { name, age, merit, other } = req.body;
         let newStudent = new Student({
             name,
             age,
-            major,
             scholarship: {
                 merit,
                 other,
@@ -77,8 +92,7 @@ app.put("/students/:_id", async (req, res) => {
                 // 所以我們需要根據client所提供的數據，來更新db內的資料
             }
         );
-
-        res.send({ msg: "成功更新學生資料！", updatedData: newData });
+        return res.render("student-update-success", { newData });
     } catch (e) {
         return res.status(400).send(e.message);
     }
